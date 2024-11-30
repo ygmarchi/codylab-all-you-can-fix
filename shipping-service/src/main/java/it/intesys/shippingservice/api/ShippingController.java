205a8b72abeebca69ce468e686491526b8edac82
Carlo Marchiori <carlo.marchiori@intesys.it>
2024-11-30 16:41:40 +0100

    Override del metodo correto v1ApiShippingPost

--- shipping-service/src/main/java/it/intesys/shippingservice/api/ShippingController.java
+++ shipping-service/src/main/java/it/intesys/shippingservice/api/ShippingController.java
@@ -1,11 +1,13 @@
 package it.intesys.shippingservice.api;
 
 import io.swagger.v3.oas.annotations.Operation;
+import it.intesys.shippingservice.api.model.OrderDTO;
 import it.intesys.shippingservice.dto.ShippingDTO;
 import it.intesys.shippingservice.service.ShippingService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,12 +19,15 @@ public class ShippingController implements V1Api {
 
     private final ShippingService shippingService;
 
-    @PostMapping("/api/shipping")
-    @Operation(summary = "Executes a new shipping")
-    public Long ship(@RequestBody @Valid ShippingDTO shippingDTO) {
+    @Override @Operation(summary = "Executes a new shipping")
+    public ResponseEntity<Long> v1ApiShippingPost(
+            @Valid @RequestBody(required = false) OrderDTO orderDTO
+    ) {
 
-        log.info("Shipping request received for order %s".formatted(shippingDTO.id()));
-        return shippingService.save(shippingDTO);
+        log.info("Shipping request received for order %s".formatted(orderDTO.getId()));
+        ShippingDTO shippingDTO = new ShippingDTO(null, orderDTO.getName(), orderDTO.getAddress(), orderDTO.getId());
+        Long shippingId = shippingService.save(shippingDTO);
+        return ResponseEntity.ok(shippingId);
     }
 
 }
